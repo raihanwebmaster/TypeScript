9b3bf67f6c686f351add96a87c6b99a4bdf09489
raihanwebmaster <raihanemon2015@gmail.com>
2022-06-16 15:38:13 +0200

    classes

--- output/script.js
+++ output/script.js
@@ -46,3 +46,17 @@ calculation = (a, b, c) => {
         return a - b;
     }
 };
+// classes
+class Players {
+    constructor(n, a, c) {
+        this.name = n;
+        this.age = a;
+        this.country = c;
+    }
+    play() {
+        console.log(`${this.name} from ${this.country} is playing`);
+    }
+}
+const mashrafi = new Players('Mashrafi', 40, 'bangladesh');
+const players = [];
+players.push(mashrafi);
--- src/script.ts
+++ src/script.ts
@@ -73,3 +73,27 @@ calculation = (a: number, b: number, c: string) => {
 }
 
 
+
+// classes
+
+class Players {
+    name: string;
+    age: number;
+    country: string;
+
+    constructor(n: string, a: number, c: string) {
+        this.name= n;
+        this.age= a;
+        this.country= c;
+    }
+
+    play() {
+        console.log(`${this.name} from ${this.country} is playing`)
+    }
+}
+
+const mashrafi = new Players('Mashrafi', 40, 'bangladesh');
+const players: Players[] = [];
+
+players.push(mashrafi);
+
